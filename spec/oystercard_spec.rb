2c500ca097117c5ed3d118a3a0c7ce13e88f479f
David <dave_carroll82@hotmail.com>
2022-01-24 17:05:46 +0000

    balance method created

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
-
-
+  it 'has a balance of zero' do
+  expect(subject.balance).to eq(0)
+  end
 end
+
+# In order to use public transport
+# As a customer
+# I want money on my card
