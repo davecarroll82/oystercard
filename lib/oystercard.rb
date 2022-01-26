ef64f65761b108a952a6a361c6456721c23be206
David <dave_carroll82@hotmail.com>
2022-01-26 09:50:26 +0000

    reduce balance method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,18 @@
 class Oystercard
+  MAX_BALANCE = 90
   
   attr_reader :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(balance)
+    fail "Maximum balance of #{MAX_BALANCE} was exceeded" if @balance + balance > MAX_BALANCE
+    @balance += balance
+  end
+
+  def spend(balance)
+    @balance -= balance
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,20 @@ require 'oystercard'
 
 describe Oystercard do
   it 'has a balance of zero' do
-  expect(subject.balance).to eq(0)
+    expect(subject.balance).to eq(0)
+    end
+
+  it 'can be topped up' do
+    expect{subject.top_up(10)}.to change {subject.balance}.by 10
+    end
+
+  it 'can reduce the balance' do
+    expect{subject.spend(10)}.to change {subject.balance}.by -10
+  end
+
+describe "change balance" do
+    it 'raise an error if max balance is reached' do
+    expect{ subject.top_up(Oystercard::MAX_BALANCE + 1) }.to raise_error("Maximum balance of #{Oystercard::MAX_BALANCE} was exceeded")
+    end
   end
 end
-
-# In order to use public transport
-# As a customer
-# I want money on my card
